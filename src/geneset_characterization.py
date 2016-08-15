80740291efbb442b425237691e049ad3cbdc76f4
XICHEN24 <xichen24@illinois.edu>
2016-08-15 16:49:38 +0000

    change import module

--- src/geneset_characterization.py
+++ src/geneset_characterization.py
@@ -6,20 +6,23 @@ Created on Wed Jul 20 14:47:45 2016
 
 def fisher(run_parameters):
     '''fisher geneset characterization'''
-    from knpackage.toolbox import run_fisher
-    run_fisher(run_parameters) 
+    # from knpackage.toolbox import run_fisher
+    import geneset_characterization_toolbox as tl
+    tl.run_fisher(run_parameters) 
     return
 
 def DRaWR(run_parameters):
     '''Discriminative Random Walk with Restart'''
-    from knpackage.toolbox import run_DRaWR
-    run_DRaWR(run_parameters)
+    # from knpackage.toolbox import run_DRaWR
+    import geneset_characterization_toolbox as tl
+    tl.run_DRaWR(run_parameters)
     return
 
 def net_one(run_parameters):
     '''net_one geneset characterization method'''
-    from knpackage.toolbox import run_net_one
-    run_net_one(run_parameters)
+    # from knpackage.toolbox import run_net_one
+    import geneset_characterization_toolbox as tl
+    tl.run_net_one(run_parameters)
     return
 
 SELECT = {
