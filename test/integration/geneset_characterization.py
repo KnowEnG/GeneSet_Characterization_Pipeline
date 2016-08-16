6d51ba911913607b1cf1f7b059fac1d9a721b2eb
XICHEN24 <xichen24@illinois.edu>
2016-08-16 20:55:03 +0000

    add timing on run fisher and run DRaWR and run net one

--- test/integration/geneset_characterization.py
+++ test/integration/geneset_characterization.py
@@ -6,7 +6,6 @@ Created on Wed Jul 20 14:47:45 2016
 import time
 def fisher(run_parameters):
     '''fisher geneset characterization'''
-    # from knpackage.toolbox import run_fisher
     import geneset_characterization_toolbox as tl
     t0 = time.time()
     tl.run_fisher(run_parameters)
@@ -15,7 +14,6 @@ def fisher(run_parameters):
 
 def DRaWR(run_parameters):
     '''Discriminative Random Walk with Restart'''
-    # from knpackage.toolbox import run_DRaWR
     import geneset_characterization_toolbox as tl
     t0 = time.time()
     tl.run_DRaWR(run_parameters)
@@ -24,9 +22,10 @@ def DRaWR(run_parameters):
 
 def net_one(run_parameters):
     '''net_one geneset characterization method'''
-    # from knpackage.toolbox import run_net_one
     import geneset_characterization_toolbox as tl
+    t0 = time.time()
     tl.run_net_one(run_parameters)
+    print('run_net_one total time: {}'.format(time.time() - t0))
     return
 
 SELECT = {
