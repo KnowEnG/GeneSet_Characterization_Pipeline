ea457dc13a265d1eda3cc416032d34ad09cacc65
XICHEN24 <xichen24@illinois.edu>
2016-08-18 10:14:31 -0500

    run pylint on geneset_characterization.py

--- src/geneset_characterization.py
+++ src/geneset_characterization.py
@@ -5,22 +5,31 @@ Created on Wed Jul 20 14:47:45 2016
 
 
 def fisher(run_parameters):
-    '''fisher geneset characterization'''
-    # from knpackage.toolbox import run_fisher
+    """fisher geneset characterization
+
+    Args:
+        run_parameters: parameter set dictionary.
+    """
     import geneset_characterization_toolbox as tl
-    tl.run_fisher(run_parameters) 
+    tl.run_fisher(run_parameters)
     return
 
 def DRaWR(run_parameters):
-    '''Discriminative Random Walk with Restart'''
-    # from knpackage.toolbox import run_DRaWR
+    """Discriminative Random Walk with Restart
+
+    Args:
+        run_parameters: parameter set dictionary.
+    """
     import geneset_characterization_toolbox as tl
     tl.run_DRaWR(run_parameters)
     return
 
 def net_one(run_parameters):
-    '''net_one geneset characterization method'''
-    # from knpackage.toolbox import run_net_one
+    """net_one geneset characterization method
+
+    Args:
+        run_parameters: parameter set dictionary.
+    """
     import geneset_characterization_toolbox as tl
     tl.run_net_one(run_parameters)
     return
@@ -31,6 +40,10 @@ SELECT = {
     "net_one":net_one}
 
 def main():
+    """
+    This the main function to characterize gene
+    set from three methods: fisher, DRaWR, net one.
+    """
     import sys
     from knpackage.toolbox import get_run_directory_and_file
     from knpackage.toolbox import get_run_parameters
