c24e1424b4844851ce59c174bcb89534ede40757
XICHEN24 <xichen24@illinois.edu>
2016-08-22 15:52:06 +0000

    change integration test

--- test/integration/geneset_characterization.py
+++ test/integration/geneset_characterization.py
@@ -4,13 +4,16 @@ Created on Wed Jul 20 14:47:45 2016
 """
 
 import time
+import yaml
+import os    
+    
 def fisher(run_parameters):
     '''fisher geneset characterization'''
     import geneset_characterization_toolbox as tl
     t0 = time.time()
     tl.run_fisher(run_parameters)
     print('run_fisher total time: {}'.format(time.time() - t0))
-    return
+    return time.time() - t0
 
 def DRaWR(run_parameters):
     '''Discriminative Random Walk with Restart'''
@@ -18,7 +21,7 @@ def DRaWR(run_parameters):
     t0 = time.time()
     tl.run_DRaWR(run_parameters)
     print('run_DRaWR total time: {}'.format(time.time() - t0))
-    return
+    return time.time() - t0
 
 def net_one(run_parameters):
     '''net_one geneset characterization method'''
@@ -26,7 +29,7 @@ def net_one(run_parameters):
     t0 = time.time()
     tl.run_net_one(run_parameters)
     print('run_net_one total time: {}'.format(time.time() - t0))
-    return
+    return time.time() - t0
 
 SELECT = {
     "fisher":fisher,
@@ -41,7 +44,11 @@ def main():
     run_directory, run_file = get_run_directory_and_file(sys.argv)
     run_parameters = get_run_parameters(run_directory, run_file)
 
-    SELECT[run_parameters["method"]](run_parameters)
+    f_time = SELECT[run_parameters["method"]](run_parameters)
+    run_parameters["running time"] = f_time
+    
+    with open(os.path.join(run_directory, run_file), 'w') as file_handle:
+        yaml.dump(run_parameters, file_handle)
 
 if __name__ == "__main__":
     main()
