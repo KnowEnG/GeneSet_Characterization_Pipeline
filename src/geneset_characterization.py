0f980df72e143acd5dcfa3e9a62b61334f87d092
XICHEN24 <xichen24@illinois.edu>
2016-08-24 20:19:29 +0000

    add timing from integration to src

--- src/geneset_characterization.py
+++ src/geneset_characterization.py
@@ -2,7 +2,10 @@
 Created on Wed Jul 20 14:47:45 2016
 @author: Xi, Suyang, Jing, Dan, Nahil
 """
-
+import sys
+import time
+import yaml
+import os   
 
 def fisher(run_parameters):
     """fisher geneset characterization
@@ -11,9 +14,10 @@ def fisher(run_parameters):
         run_parameters: parameter set dictionary.
     """
     import geneset_characterization_toolbox as tl
+    t0 = time.time()
     tl.run_fisher(run_parameters)
-    return
-
+    return time.time() - t0
+    
 def DRaWR(run_parameters):
     """Discriminative Random Walk with Restart
 
@@ -21,8 +25,9 @@ def DRaWR(run_parameters):
         run_parameters: parameter set dictionary.
     """
     import geneset_characterization_toolbox as tl
+    t0 = time.time()
     tl.run_DRaWR(run_parameters)
-    return
+    return time.time() - t0
 
 def net_one(run_parameters):
     """net_one geneset characterization method
@@ -31,8 +36,9 @@ def net_one(run_parameters):
         run_parameters: parameter set dictionary.
     """
     import geneset_characterization_toolbox as tl
+    t0 = time.time()
     tl.run_net_one(run_parameters)
-    return
+    return time.time() - t0
 
 SELECT = {
     "fisher":fisher,
@@ -44,14 +50,18 @@ def main():
     This the main function to characterize gene
     set from three methods: fisher, DRaWR, net one.
     """
-    import sys
+
     from knpackage.toolbox import get_run_directory_and_file
     from knpackage.toolbox import get_run_parameters
 
     run_directory, run_file = get_run_directory_and_file(sys.argv)
     run_parameters = get_run_parameters(run_directory, run_file)
 
-    SELECT[run_parameters["method"]](run_parameters)
+    f_time = SELECT[run_parameters["method"]](run_parameters)
+    run_parameters["running_time"] = f_time
+    
+    with open(os.path.join(run_directory, run_file), 'w') as file_handle:
+        yaml.dump(run_parameters, file_handle, default_flow_style=False)
 
 if __name__ == "__main__":
     main()
