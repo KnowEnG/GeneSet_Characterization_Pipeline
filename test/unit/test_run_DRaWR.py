9f081dfa121ca02558fe7af0efb75bb964c5140d
XICHEN24 <xichen24@illinois.edu>
2016-09-09 14:34:54 -0500

    add unit test run dir

--- test/unit/test_run_DRaWR.py
+++ test/unit/test_run_DRaWR.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class TestRun_DRaWR(TestCase):
     def setUp(self):
-        with open("tmp/DRaWR_run_file.yml", 'r') as file_handle:
+        with open("unit_test_run_dir/DRaWR_run_file.yml", 'r') as file_handle:
             self.run_parameters = yaml.load(file_handle)
 
     def tearDown(self):
