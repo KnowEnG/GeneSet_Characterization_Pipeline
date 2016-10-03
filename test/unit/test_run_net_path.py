412a8c8bdcbb131b98380bfa96c78085e9dd91a6
Xi Chen <xichen@wirelessprv-10-194-143-195.near.illinois.edu>
2016-10-03 17:27:33 -0500

    add run net path unittest

---
+++ test/unit/test_run_net_path.py
@@ -0,0 +1,28 @@
+import yaml
+import unittest
+from unittest import TestCase
+import geneset_characterization_toolbox as tl
+import pandas as pd
+import numpy as np
+
+
+class TestRun_net_path(TestCase):
+    def setUp(self):
+        with open("unit_test_run_dir/net_path_run_file.yml", 'r') as file_handle:
+            self.run_parameters = yaml.load(file_handle)
+
+    def tearDown(self):
+        del self.run_parameters
+
+    def test_net_path(self):
+        ret = tl.run_net_path(self.run_parameters)
+        ret.index = np.arange(ret.shape[0])
+        print(ret)
+        res = pd.DataFrame([['P6', 'P7'],['P7', 'P6']], columns=['user1', 'user2'])
+        print(res)
+        comp = ret.equals(res)
+        self.assertEqual(True, comp)
+
+if __name__ == '__main__':
+    unittest.main()
+
