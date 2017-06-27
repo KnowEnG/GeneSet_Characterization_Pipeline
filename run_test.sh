daa6089ed7552c6b8d5053caadbf8a3a18205784
candicegjing <candicegjing@gmail.com>
2017-06-27 09:37:12 -0700

    Update shell script

--- run_test.sh
+++ run_test.sh
@@ -3,9 +3,6 @@ echo "Hello, World!"
 echo "Knowledge is power."
 uname -a
 sudo apt-get install build-essential
-#echo $PATH
-#export PATH=$PATH:/usr/local/bin/make
-#echo $PATH
 cd test
 pwd
 make verification_tests
